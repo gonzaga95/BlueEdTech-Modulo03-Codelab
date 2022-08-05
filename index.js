98acec3d4771e096c567726f5251779f23f0bbeb
Carlos Eduardo Gonzaga <91717487+gonzaga95@users.noreply.github.com>
2022-08-04 21:20:48 -0400

    algum problema no index.js

--- index.js
+++ index.js
@@ -9,16 +9,6 @@ app.use(express.json());
 app.use(cors());
 app.use('/termos', routes);
 
-// app.get('/termos/get-all', (req, res) => {
-//     res.send(termos);
-// });
-
-// app.get('/termos/get-all/:id', (req, res) => {
-//     const idParam = req.params.id;
-//     const escolhaTermo = termos.find((termo) => termo.id == idParam);
-//     res.send(escolhaTermo);
-// });
-
 app.listen(port, () => {
     console.log(`A aplicação está rodando na porta http://localhost:${port}/`);
 });
