0a7377ec5631ebd96616941ac9d98259cbea85c1
Carlos Eduardo Gonzaga <91717487+gonzaga95@users.noreply.github.com>
2022-08-11 20:18:52 -0400

    importando db e chamando no index

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const port = 3000;
+const connectMongoDb = require('./src/database/database');
 
 const routes = require('./src/routes/termo.route');
 const app = express();
@@ -8,6 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use('/termos', routes);
+
+connectMongoDb();
 
 app.listen(port, () => {
     console.log(`A aplicação está rodando na porta http://localhost:${port}/`);
