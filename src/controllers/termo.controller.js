0c553b98869137e6d137f5ba3ed5763f28480ede
Carlos Eduardo Gonzaga <91717487+gonzaga95@users.noreply.github.com>
2022-08-11 22:14:47 -0400

    controller com mongoose e tratamento de erros

--- src/controllers/termo.controller.js
+++ src/controllers/termo.controller.js
@@ -1,33 +1,73 @@
 const termosSerivce = require('../services/termo.service');
+const mongoose = require('mongoose');
 
-const findAllTermosController = (req, res) => {
-    const termos = termosSerivce.findAllTermosService();
-    res.send(termos);
+const findAllTermosController = async (req, res) => {
+    const allTermos = await termosSerivce.findAllTermosService();
+
+    if (allTermos.length == 0) {
+        res.status(404).send({ message: 'Não existe nenhum termo cadastrado' });
+    }
+
+    res.status(200).send(allTermos);
 };
 
-const findByIdTermoController = (req, res) => {
-    const parametroId = Number(req.params.id);
-    const escolhaTermo = termosSerivce.findByIdTermoService(parametroId);
-    res.send(escolhaTermo);
+const findByIdTermoController = async (req, res) => {
+    const parametroId = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(parametroId)) {
+        return res.status(400).send({ message: 'ID não encontrado' });
+    }
+
+    const termoEscolhido = await termosSerivce.findByIdTermoService(
+        parametroId,
+    );
+    res.status(200).send(termoEscolhido);
 };
 
-const createTermoController = (req, res) => {
+const createTermoController = async (req, res) => {
     const termo = req.body;
-    const newTermo = termosSerivce.createTermoService(termo);
-    res.send(newTermo);
+
+    if (!termo || !termo.descricao || !termo.logo || !termo.ano_de_criacao) {
+        return res.status(400).send({
+            message: 'Todos os campos precisam ser preenchidos',
+        });
+    }
+
+    const newTermo = await termosSerivce.createTermoService(termo);
+    res.status(201).send(newTermo);
 };
 
-const editTermoController = (req, res) => {
-    const idParam = Number(req.params.id);
+const editTermoController = async (req, res) => {
+    const parametroId = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(parametroId)) {
+        return res.status(400).send({ message: 'ID não encontrado' });
+    }
+
     const termoEdit = req.body;
-    const updatedTermo = termosSerivce.editTermoService(idParam, termoEdit);
-    res.send(updatedTermo);
+
+    if (!termo || !termo.descricao || !termo.logo || !termo.ano_de_criacao) {
+        return res.status(400).send({
+            message: 'Todos os campos precisam ser preenchidos',
+        });
+    }
+
+    const updatedTermo = await termosSerivce.editTermoService(
+        parametroId,
+        termoEdit,
+    );
+    res.status(200).send(updatedTermo);
 };
 
-const deleteTermoController = (req, res) => {
-    const idParam = req.params.id;
-    termosSerivce.deleteTermoService(idParam);
-    res.send({ message: 'Termo deletado com sucesso!' });
+const deleteTermoController = async (req, res) => {
+    const parametroId = req.params.id;
+
+    if (!mongoose.Types.ObjectId.isValid(parametroId)) {
+        return res.status(400).send({ message: 'ID não encontrado' });
+    }
+
+    await termosSerivce.deleteTermoService(parametroId);
+    res.status(202).send({ message: 'Termo deletado com sucesso!' });
 };
 
 module.exports = {
